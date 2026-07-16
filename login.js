f811c8e24508f63046233fb5d2f4e0a951c20641
malak202521 <malaktameralb@gmail.com>
2026-07-16 11:48:11 +0300

    Refactor login.js for clarity and functionality

--- login.js
+++ login.js
@@ -71,7 +71,7 @@ document.addEventListener('DOMContentLoaded', function() {
             window.location.href = "pro-student2.html";
             break;
           case "student2":
-            window.location.href = "student.html";
+            window.location.href = "PRO student.html";
             break;
           case "facultym":
             window.location.href = "FacultyMember.html";
