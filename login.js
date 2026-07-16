d1e4efed4a17e5f76b42b5661beb856583895212
malak202521 <malaktameralb@gmail.com>
2026-07-16 11:43:18 +0300

    Refactor login.js for improved readability and structure

--- login.js
+++ login.js
@@ -71,7 +71,7 @@ document.addEventListener('DOMContentLoaded', function() {
             window.location.href = "pro-student2.html";
             break;
           case "student2":
-            window.location.href = "PROSTUDENT.html";
+            window.location.href = "student.html";
             break;
           case "facultym":
             window.location.href = "FacultyMember.html";
